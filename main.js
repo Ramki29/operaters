adf4c406e24398a57cacc79c0ed172c9adfa0fca
Ramki29 <ramkie7010@gmail.com>
2023-03-13 09:48:41 +0530

    add operaters

---
+++ main.js
@@ -0,0 +1,96 @@
+var b=10;
+var a=5;
+console.log(a+b);
+
+
+let x=20;
+console.log(x);
+
+var dob;
+console.log(typeof dob);
+
+var a=null;
+console.log(typeof a);
+
+
+var a="20";
+var b="11";
+console.log(a+b);
+
+let d=9;
+let e="10";
+console.log(d+e);
+
+let i=10
+let h=5;
+console.log(i+h);
+
+//arithmetic operators
+
+var a=5, b=3, c=4;
+console.log(a+b+c);
+
+console.log(a+b-c);
+
+console.log(a+b*c);
+
+console.log(a+b/c);
+
+console.log(a+b%c);
+
+console.log(a-b*c);
+
+console.log(a*b/c);
+
+console.log(a/b%c);
+
+console.log(a%b/c);
+
+console.log(a-b%c);
+
+console.log(a+b**c);
+
+console.log(a%b**c);
+
+console.log(a-b**c);
+
+console.log(a++);
+
+
+console.log(++b + c);
+
+console.log(c--);
+
+console.log(a++);
+
+console.log(++b);
+
+console.log(a++ + a*c);
+
+console.log(c--);
+
+console.log(--c);
+
+console.log(--c +a);
+
+
+//comparision operators
+
+var j=5, k=4, l=3;
+console.log(j>k);
+
+console.log(l>k);
+
+console.log(j>=k);
+
+console.log(j<=k);
+
+console.log(j==k);
+
+console.log(j!=k);
+
+console.log(j===k);
+
+console.log(j!==k);
+
+console.log(j=k);
